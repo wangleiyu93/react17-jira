16e963c11ae66e46d4fb01330e79ec3b5d9cbd32
wangleiyu <spider.wang@tron.network>
2024-01-23 11:10:50 +0800

    chore: js -> ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,36 +1,39 @@
 import { useEffect, useState } from "react";
 
-export const isFalsy = (value) => (value === 0 ? false : !value);
+export const isFalsy = (value:unknown) => (value === 0 ? false : !value);
 
-export const cleanObject = (object) => {
+export const cleanObject = (object:object) => {
   const result = { ...object };
   Object.keys(object).forEach((key) => {
+    //@ts-ignore
     if (isFalsy(object[key])) {
+       //@ts-ignore
       delete result[key];
     }
   });
   return result;
 };
 
-export const useMount = (callback) => {
+export const useMount = (callback:()=>void) => {
   useEffect(() => {
     callback();
   }, []);
 };
 
-export const debounce = (func, delay) => {
-  let timeout;
-  return (...param) => {
+export const debounce = (func:()=>void, delay:number) => {
+  let timeout:any;
+  return (...param: any) => {
     if (timeout) {
       clearTimeout(timeout);
     }
     timeout = setTimeout(() => {
+       //@ts-ignore
       func(...param);
     }, delay);
   };
 };
 
-export const useDebounce = (value, delay) => {
+export const useDebounce = (value:unknown, delay:number):any => {
   const [debouncedValue, setDebouncedValue] = useState(value);
   useEffect(() => {
     const timeout = setTimeout(() => {
